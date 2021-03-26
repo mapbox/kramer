ed494191732d638a4051f33b906125de80688118
David Clark <dclark@mapbox.com>
2021-03-26 10:20:46 -0700

    Update dependencies, remove footnote-style links (#1)

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ exports.settings = {
 };
 
 exports.plugins = [
+  // Support GitHub Flavored Markdown.
+  require('remark-gfm'),
+
   // Accept frontmatter.
   require('remark-frontmatter'),
 
-  // Format (beyond default settings).
+  // Formatting beyond the remark-stringify settings.
   [require('remark-toc'), { maxDepth: 3, tight: true }],
   require('remark-inline-links'),
-  require('remark-reference-links'),
 
   // Lint.
   require('remark-validate-links'),
   [require('remark-lint-blockquote-indentation'), 2],
-  [require('remark-lint-file-extension'), 'md'],
   require('remark-lint-final-definition'),
   require('remark-lint-no-duplicate-definitions'),
   require('remark-lint-no-multiple-toplevel-headings'),
-  require('remark-lint-no-tabs'),
   require('remark-lint-no-undefined-references'),
 ];
